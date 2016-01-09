64f0064404277259a6028a6f45dec9e44f4fbc88
Matt Morgis <matt@rappiddevelopment.com>
2016-01-09 14:14:17 -0500

    Adding server component

---
+++ server.js
@@ -0,0 +1,34 @@
+/**
+  * General setup of the web application. 
+  * Import express and other stand libraries 
+  * to get up and running.
+  */
+
+var express  = require('express'),
+    path     = require('path'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+/**
+  * Set EJS template Engine
+  * EJS let's us use variables in-line
+  * with our markup and renders it to HTML
+  * for us.
+  */
+// app.set('views','./views');
+// app.set('view engine','ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true })); //support x-www-form-urlencoded
+app.use(bodyParser.json());
+
+app.set('port', process.env.PORT || 3000);
+app.set('host', 'localhost');
+
+app.get('/', function(req, res) {
+  res.send('index.html');
+});
+
+app.listen(app.get('port'), app.get('host'), function() {
+    console.log('1617 Barber Life has started listening at http://' + app.get('host') + ':' + app.get('port') + '. ^C to stop.');
+});
